0cb9d0ee2443e130cfc7a225d73ad4bf6a52dbc0
Sayan Datta <sdcreativity12@gmail.com>
2026-07-31 23:49:56 +0530

    feat: add root template with ViewTransition wrapper for page animations

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -2,7 +2,6 @@
 
 import * as React from 'react';
 import { ViewTransition } from 'react';
-import { usePathname } from 'next/navigation';
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return <ViewTransition name="page">{children}</ViewTransition>;
